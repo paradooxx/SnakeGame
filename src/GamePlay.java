d5411f45a13547c6222b19537bfd8b2506a9b4e9
theParas <parassth63@gmail.com>
2022-05-22 18:42:42 +0545

    updated icons

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -2,9 +2,27 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.ImageIcon;
 
 public class GamePlay extends JPanel{
+	
+	private int[] snakeXlength = new int[750];
+	private int[] snakeYlength = new int[750];
+	
+	private boolean top = false;
+	private boolean bottom = false;
+	private boolean right = false;
+	private boolean left = false;
+	
+	private Timer timer;
+	private int delay = 100;
+	
+	private ImageIcon upMouth;
+	private ImageIcon bottomMouth;
+	private ImageIcon rightMouth;
+	private ImageIcon leftMouth;
+	private ImageIcon snakeImage;
 	
 	private ImageIcon titleImage;
 	public GamePlay()
@@ -25,5 +43,8 @@ public class GamePlay extends JPanel{
 		g.drawRect(24, 73, 1214, 600);
 		g.setColor(Color.DARK_GRAY);
 		g.fillRect(25, 75, 1213, 598);
+		
+		rightMouth = new ImageIcon("images/rightmouth.png");
+		rightMouth.paintIcon(this, g, 500, 500);
 	}
 }
